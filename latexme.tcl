820c57f4b8a15d6b2230c4c4d5ed5d656660ef90
LucDachary <LucDachary@users.noreply.github.com>
2023-06-30 17:54:14 +0200

    Adds the -build option.

--- latexme.tcl
+++ latexme.tcl
@@ -2,17 +2,33 @@
 package require Tcl 8.5
 package require cmdline 1.5.2
 
-if { $argc != 2 } {
-	puts "error: I need exactly two arguments\n"
-	puts "USAGE:"
-	puts "\t$argv0 CLASS FILENAME\n"
-	puts "EXEMPLE:"
-	puts "\t$argv0 article new-article.tex"
+set options {
+	{class.arg "article" "the Latex document's class"}
+	{build "build the document after its creation"}
+}
+set usage ": latexme \[options] filename\noptions:"
+
+try {
+	array set params [::cmdline::getoptions argv $options $usage]
+	# Note: argv is modified now. The recognized options are removed from it
+	# leaving the non-option arguments behind.
+
+	# Only 1 argument is expected now, others are options.
+	if { [llength $argv] != 1 } {
+		puts "error: I need exactly one argument, got [llength $argv]\n"
+		puts "USAGE:"
+		puts "\t$argv0 \[options\] FILENAME\n"
+		puts "EXEMPLE:"
+		puts "\t$argv0 new-article.tex"
+		exit 1
+	}
+} trap {CMDLINE USAGE} {msg o} {
+	puts $msg
 	exit 1
 }
 
 # TODO if the filename does not end in ".tex", add the suffix?
-set output_filepath [lindex $argv 1]
+set output_filepath [lindex $argv 0]
 if [file exists $output_filepath] {
 	puts "This exists already; doing nothing."
 	exit 1
@@ -20,7 +36,7 @@ if [file exists $output_filepath] {
 cd [file dirname $output_filepath]
 
 # TODO add support for "report", "beamer", "scrlttr2".
-set document_class [lindex $argv 0]
+set document_class $params(class)
 if { $document_class != "article" } {
 	puts {error: for now only the class "article" is supported.}
 	exit 1
@@ -36,20 +52,22 @@ exec cp [file join $templates_dirpath "article.tex"] $output_filepath
 puts "Latex \"$document_class\" document has been created at \"$output_filepath\"."
 
 # TODO add a commandline option to execute this part.
-puts "Building…"
-set build_dirname "build"
-if {[file exists $build_dirname] && ![file isdirectory $build_dirname]} {
-	puts "\"$build_dirname\" must be a directory and is not; doing nothing."
-	exit 1
-}
-file mkdir $build_dirname
+if { $params(build) } {
+	puts "Building…"
+	set build_dirname "build"
+	if {[file exists $build_dirname] && ![file isdirectory $build_dirname]} {
+		puts "\"$build_dirname\" must be a directory and is not; doing nothing."
+		exit 1
+	}
+	file mkdir $build_dirname
 
-set CTRL_D \004
+	set CTRL_D \004
 
-spawn xelatex --output-directory build $output_filepath
-expect {
-	# TODO exit with error code?
-	{Please type another input file name} {send $CTRL_D}
-	{Package fontspec Error} {send $CTRL_D}
-	default {puts "Document is built!"}
+	spawn xelatex --output-directory build $output_filepath
+	expect {
+		# TODO exit with error code?
+		{Please type another input file name} {send $CTRL_D}
+		{Package fontspec Error} {send $CTRL_D}
+		default {puts "Document is built!"}
+	}
 }
